666a9cfe0dcac51498977395568f2be310e48d82
mzeht <m18989498303@163.com>
2017-01-21 15:38:16 +0800

    配置数据库加密

--- src/main/java/com/wpmac/mzehtapi/resource/MainController.java
+++ src/main/java/com/wpmac/mzehtapi/resource/MainController.java
@@ -1,10 +1,13 @@
 package com.wpmac.mzehtapi.resource;
 
+import com.alibaba.druid.filter.config.ConfigTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Administrator on 2016/9/22.
@@ -17,7 +20,34 @@ public class MainController {
 
     @RequestMapping(value = "/listActionLog", method = RequestMethod.GET)
     public String hello() {
+
+
+        //密码明文
+        String password = "123456";
+        System.out.println("密码[ "+password+" ]的加密信息如下：\n");
+
+        String [] keyPair = new String[0];
+        try {
+            keyPair = ConfigTools.genKeyPair(512);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+//私钥
+        String privateKey = keyPair[0];
+//公钥
+        String publicKey = keyPair[1];
+//用私钥加密后的密文
+        try {
+            password = ConfigTools.encrypt(privateKey, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("privateKey:"+privateKey);
+        System.out.println("publicKey:"+publicKey);
+        System.out.println("password:"+password);
         return "list_action_log";
+
     }
 
     /**
